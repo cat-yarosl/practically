4896e51f39ddcf9be5af6b6ea354442bb4fa5762
catheriny09 <catheriny09@gmail.com>
2024-01-23 21:46:13 -0800

    styling

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,12 +8,14 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
     return (
         <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
+            <hr></hr>
             <ul className={`options ${collapsed ? 'collapsed' : ''}`}>
                 <li><Link to='/'>{collapsed ? '' : 'Home'}</Link></li>
                 <li><Link to='/about'>{collapsed ? '' : 'About'}</Link></li>
                 <li><Link to='/projects'>{collapsed ? '' : 'Projects'}</Link></li>
                 <li><Link to='/contact'>{collapsed ? '' : 'Contact'}</Link></li>
             </ul>
+            <hr></hr>
         </div>
     );
 };
