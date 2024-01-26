ca4807ee8f004f7b7555855e000a9a64a8cfc18e
catheriny09 <catheriny09@gmail.com>
2024-01-25 19:29:49 -0800

    adding content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 function Home() {
     return (
         <div className='content'>
-            <h2>Greetings!</h2>
-            <p>Welcome to my portfolio page. Feel free to look around!</p>
+            <div className='left-home'>
+                <h2>Greetings!</h2>
+                <p>Welcome to my portfolio page. Feel free to look around!</p>
+            </div>
+            <div className='right-home'>
+                <p>dssdfsdfwdfsdfs</p>
+            </div>
         </div>
     );
 }
