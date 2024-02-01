bb45699bb0e0cd3f4d92034b3ebf425d44a89005
catheriny09 <catheriny09@gmail.com>
2024-02-01 15:48:16 -0800

    adding home page content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,15 @@ const Home: React.FC = () => {
                 <p>Welcome to my portfolio page. Feel free to look around!</p>
             </div>
             <div className='right-home'>
-                <p>dssdfsdfwdfsdfs</p>
+                <hr></hr>
+                <p>I am a software developer who is looking for new opportunities. 
+                    In particular, I am passionate about the potential applications of 
+                    artificial intelligence in modern technology. I believe it can be used
+                    as a tool to assist people on both a professional and day-to-day basis. 
+                    Some of the artwork on this page, including the header, was made with the
+                    assistance of artificial intelligence!
+                    Additionally, I am adept at data analysis and processing. 
+                </p>
             </div>
         </div>
     );
