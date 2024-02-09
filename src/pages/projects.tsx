8ece98a67ba81d2cf7c8756fbe9dbd3ff174a677
catheriny09 <catheriny09@gmail.com>
2024-02-08 22:44:55 -0800

    adding projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Projects: React.FC = () => {
     return (
         <div className='cards'>
-            <h1>Projects</h1>
+             <div className="project-card">
+                <div className="contact-header">
+                    <h2> {'>'} Weatherly</h2>
+                </div>
+                <hr></hr>
+                <div className="contact-body">
+                </div>
+            </div>
+            <div className="project-card">
+                <div className="contact-header">
+                    <h2> {'>'} Gistify</h2>
+                </div>
+                <hr></hr>
+                <div className="contact-body">
+                </div>
+            </div>
         </div>
     );
 }
